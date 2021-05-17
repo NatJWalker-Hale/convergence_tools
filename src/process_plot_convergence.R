98ed5a129fd0d5a444d1b6d9b3035071ae06ea2a
natjwalkerhale <nathanael.walker.hale@gmail.com>
2021-05-17 23:36:45 +0100

    updates

--- src/process_plot_convergence.R
+++ src/process_plot_convergence.R
@@ -7,7 +7,7 @@ setwd("~/Dropbox/cary_projects/DODA/cary/new_conv_2021/strict/recon_topo/") # se
 
 # diffsel
 
-meandiffsel <- read.csv("br_part_1st_origin/diffsel/2cond/run1_cln_1.meandiffsel", sep = "\t", header = F)[,1:21]
+meandiffsel <- read.csv("br_part_special/diffsel/2cond/run1_cln_1.meandiffsel", sep = "\t", header = F)[,1:21]
 colnames(meandiffsel) <- c("pos",'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y')
 score <- t(apply(meandiffsel, 1, function(x) {2 * abs(0.5 - x[2:21])}))
 meandiffsel$max <- apply(score, 1, max)
@@ -15,18 +15,18 @@ meandiffsel$max_aa <- apply(score, 1, function(x) {colnames(score)[which.max(x)]
 
 # msd
 
-msd <- read.csv("br_part_original/msd/output.txt", sep="\t", skip=3, header=F)
+msd <- read.csv("br_part_original/prank/msd/output.txt", sep="\t", skip=3, header=F)
 msd <- msd[order(msd$V1),]
 msd$score <- 1-as.numeric(msd$V2)
 
 # PCOC
 
-pcoc <- read.csv("br_part_1st_origin/PCOC/prank_PCOC_C60_aa_brlen_unroot_exclude/RUN_20210429_094808/DODAa_combined_no_og_strict_for_synth.cds.fa.nostop.name.noF.best.fas.results.tsv",
+pcoc <- read.csv("br_part_special/PCOC/prank_PCOC_C60_aa_brlen_unroot/RUN_20210430_084954/DODAa_combined_no_og_strict_for_synth.cds.fa.nostop.name.noF.best.fas.results.tsv",
                  sep="\t", header=T) # no transformations necessary
 
 # tdg09
 
-out <- yaml.load_file("br_part_1st_origin/tdg09/tdg09_prank_JTT+G_exclude_out.txt")
+out <- yaml.load_file("br_part_special/prank/tdg09/prank_JTT+G_out.txt")
 tdg09 <- as.data.frame(matrix(unlist(out$FullResults), ncol=9, byrow=T))
 colnames(tdg09) <- c("pos","ssFparam","ssFlogL","lssFparam","lssFlogL","deltaLogL","df","LRT","FDR")
 tdg09$score <- 1-as.numeric(tdg09$FDR)
@@ -34,7 +34,7 @@ tdg09$score[is.na(tdg09$score)] <- 0.0
 
 # topo
 
-topo <- read.csv("br_part_1st_origin/topo/prank_topo_out.txt", header=T)
+topo <- read.csv("br_part_special/prank/topo/prank_JTT+G_topo_out.txt", header=T)
 
 
 # all
@@ -48,6 +48,11 @@ results <- data.frame(pos=pcoc$Sites,
                       )
 
 results$pass <- apply(results[,2:6],1,function(x) {sum(x > 0.9)})
+
+#write.csv(x = results, "br_part_original/prank/all_results.csv", row.names = F)
+#write.csv(x = results, "br_part_1st_origin/prank/all_results.csv", row.names = F)
+#write.csv(x = results, "br_part_special/prank/all_results.csv", row.names = F)
+#write.csv(x = results,"br_part_original_inverse/prank/all_results.csv", row.names = F)
 
 cols <- brewer.pal(5, "Set2")
 # cols <- viridis(5)
@@ -84,47 +89,37 @@ for (i in 1:max(results$pos)) {
   # }
 }
 
-par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
-plot(0, 0, type = 'l', bty = 'n', xaxt = 'n', yaxt = 'n')
-#legend("bottom",legend=c("diffsel","msd","pcoc","tdg09","topo"), col = cols)
-legend('bottom',legend=c("diffsel","msd","pcoc","tdg09","topo"), col=cols, lwd = 5, xpd = TRUE, horiz = TRUE, cex = 1, seg.len=1, bty = 'n')
+require(VennDiagram)
 
+# prank
+df <- read.csv("br_part_original/prank/all_results.csv", header=T)
+original_pass3 <- df[which(df$pass >= 3),]$pos
+df <- read.csv("br_part_1st_origin/prank/all_results.csv", header=T)
+first_origin_pass3 <- df[which(df$pass >= 3),]$pos
+df <- read.csv("br_part_special/prank/all_results.csv", header=T)
+special_pass3 <- df[which(df$pass >= 3),]$pos
+df <- read.csv("br_part_original_inverse/prank/all_results.csv", header=T)
+inverse_pass3 <- df[which(df$pass >= 3),]$pos
 
-(Y <- grconvertY(0.9, "user", "ndc"))
-pushViewport(viewport())
-grid.lines(x = c(0,4), y = Y, gp = gpar(lty=3, col = "black"))
-popViewport()
+sites <- list(Original=original_pass3, First=first_origin_pass3, Special=special_pass3, Inverse=inverse_pass3)
 
-# nsites <- max(meandiffsel$pos) # 274 in cleaned PRANK alignment
-# # 55 sites at a time, 55, 55, 55, 55, 55
-# par(mfrow=c(5,1), mar=c(2.0,0.5,0.5,0.5))
-# plot(meandiffsel$max[1:55], type="h", lwd=1, main="", xlab="", ylab="",
-#      xlim=c(1,55))
-# abline(0.9,0,lty=3)
-# # points(which(meandiffsel$max[1:55] > 0.9), meandiffsel$max[meandiffsel$max > 0.9][1:55],
-# #        pch=20)
-# plot(meandiffsel$max[56:110], type="h", lwd=1, main="", xlab="", ylab="",
-#      xaxt="n")
-# axis(1, at=seq(10,50,10), labels=c(65,75,85,95,105))
-# abline(0.9,0,lty=3)
-# # points(which(meandiffsel$max[56:110] > 0.9), meandiffsel$max[meandiffsel$max[56:110] > 0.9],
-# #        pch=20)
-# plot(meandiffsel$max[111:165], type="h", lwd=1, main="", xlab="", ylab="",
-#      xaxt="n")
-# axis(1, at=seq(10,50,10), labels=c(120,130,140,150,160))
-# abline(0.9,0,lty=3)
-# # points(which(meandiffsel$max[111:165] > 0.9), meandiffsel$max[meandiffsel$max > 0.9][56:110],
-# #        pch=20)
-# plot(meandiffsel$max[166:220], type="h", lwd=1, main="", xlab="", ylab="",
-#      xaxt="n")
-# axis(1, at=seq(10,50,10), labels=c(175,185,195,205,215))
-# abline(0.9,0,lty=3)
-# # points(which(meandiffsel$max > 0.9), meandiffsel$max[meandiffsel$max > 0.9],
-# #        pch=20)
-# plot(meandiffsel$max[221:273], type="h", lwd=1, main="", xlab="", ylab="",
-#      xaxt="n")
-# axis(1, at=seq(10,50,10), labels=c(230,240,250,260,270))
-# abline(0.9,0,lty=3)
-# # points(which(meandiffsel$max > 0.9), meandiffsel$max[meandiffsel$max > 0.9],
-# #        pch=20)
+venn.diagram(sites, filename = "test.png")
 
+display_venn <- function(x, ...){
+  grid.newpage()
+  venn_object <- venn.diagram(x, filename = NULL, ...)
+  grid.draw(venn_object)
+}
+
+display_venn(sites,
+             fill = brewer.pal(4, "Set2"),
+             lwd=2,
+             lty="blank",
+             sub.fontface="arial",
+             sub.fontfamily="sanserif")
+
+inAll <- intersect(intersect(intersect(original_pass3,first_origin_pass3),special_pass3),inverse_pass3)
+uniq_original <- c(78)
+uniq_special <- c(NA)
+uniq_inverse <- c(183)
+uniq_first <- c(79,81,148)
